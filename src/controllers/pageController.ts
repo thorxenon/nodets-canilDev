6b2c41d47eacc944fae194e39fc95c6aad8f8f7d
Pedro Lucas <pedrolucasrangel1@hotmail.com>
2021-11-27 11:44:38 -0300

    base created

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,47 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/cresteMenuObject';
 
 export const home = (req: Request, res: Response) =>{
-    res.send('home');
-    //res.render('/pages/home');
+
+    res.render('pages/page',{
+        menu:createMenuObject('all'),
+
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 };
 
 export const dogs = (req: Request, res: Response) =>{
-    res.send('dogs');
-    //res.render(/pages/dogs);
+
+    res.render('pages/page',{
+        menu:createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 };
 
 export const cats = (req: Request, res: Response) =>{
-    res.send('cats');
-    //res.render(/pages/cats);
+
+    res.render('pages/page',{
+        menu:createMenuObject('cat'),
+        banner: {
+            title: 'Cats',
+            background: 'banner_cat.jpg'
+        }
+    });
 };
 
 export const fishes = (req: Request, res: Response) =>{
-    res.send('fishes');
-    //res.render(/pages/fishes);
+
+    res.render('pages/page',{
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 };
